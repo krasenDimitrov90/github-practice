13fb61fd3f213196ae33c8f7e970f43f159ad22d
krasenDimitrov90 <krasendimitrov90@gmail.com>
2023-05-22 07:33:16 +0300

    added cart vesability state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useState } from 'react';
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+  const showCartHandler = () => {
+    setCartIsShown(true);
+    console.log('Show Cart');
+  };
+  const hideCartHandler = () => setCartIsShown(false);
+
   return (
     <CartProvider>
-      <Header />
+      {cartIsShown && <div>Cart...</div>}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
